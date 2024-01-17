a3e3ef7ef3740f821f55921958c63bd34cd15997
Samir Guragain <samirguragain9@gmail.com>
2024-01-17 13:04:45 +0545

    changing thumbnails and increment buttons are now funcitonal

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,8 +1,47 @@
-const small = document.getElementsByClassName("small");
+const small = [...document.getElementsByClassName("small")];
+const mainImg = document.getElementsByClassName("mainImg")[0];
+const qantity = document.getElementById("quantity");
+const addCartBtn = document.getElementById("addToCart");
+const cartBtn = document.getElementById("cart");
 
+//iterations for the product image view area.....
 
-for(let i = 0; i < small.length; i++){
-    small[i].style.backgroundImage  = `url("images/image-product-${i + 1}-thumbnail.jpg")`;
+small.forEach((item, index) => {
+
+    item.style.backgroundImage = `url("images/image-product-${index + 1}-thumbnail.jpg")`;
+
+    item.addEventListener("click", () => {
+        mainImg.style.backgroundImage = `url("images/image-product-${index + 1}.jpg")`;
+    })
+});
+
+// now for the increment and decrement button for the add to cart section
+
+let count = 0;
+
+function plus(){
+    count++;
+    quantity.innerHTML = count;
 }
+
+function minus(){
+    count--;
+    if(count <= 0){
+        count = 0;
+    }
+    quantity.innerHTML = count;
+}
+
+
+// add to cart fucntionings 
+
+addCartBtn.addEventListener("click", () => {
+    
+})
+
+
+
+
+
 
 
